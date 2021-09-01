a257f072c912de524c76514be5cdf5f3227cf89b
Davi Leme <leme.davi470@gmail.com>
2021-09-01 17:42:33 -0300

    First Commit

---
+++ src/main/java/br/com/davi/restapivendas/RestApiVendasApplication.java
@@ -0,0 +1,13 @@
+package br.com.davi.restapivendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiVendasApplication.class, args);
+	}
+
+}
